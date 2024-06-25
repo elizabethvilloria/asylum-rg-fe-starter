1b001cfbb72254a155d4435bb58fce2baf13a78a
elizabethvilloria <lizvilloria@gmail.com>
2024-06-25 18:53:12 +0200

    Added CSS for bottom section

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -10,8 +10,7 @@ import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// 
-import PageNav from '../../common/PageNav';
+// import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -22,6 +21,7 @@ function RenderLandingPage(props) {
   const history = useHistory();
 
   // #2: added images section
+  // #4: added bottom section
   return (
     <div className="main">
       <div className="header">
@@ -79,8 +79,27 @@ function RenderLandingPage(props) {
         </div>
       </div>
       <div>
+
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+        <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <div className='insights'>
+            <div className='insight'>
+              <h2>36%</h2>
+              <p>By the end of the Trump administration, the average asylum office grant rate had fallen 36 percent from an average of 44 percent in fiscal year 2016 to 28 percent in fiscal year 2020</p>
+            </div>
+            <div className='insight'>
+              <h2>5%</h2>
+              <p>The New York asylum office grant rate dropped to 5 percent in fiscal year 2020</p>
+            </div>
+            <div className='insight'>
+              <h2>6x Lower</h2>
+              <p>Between fiscal year 2017 and 2020, the New York asylum office's average grant rate was six times lower than the San Francisco asylum office</p>
+            </div>
+          </div>
+        </div>
         {/* <div className="bottom-section">*/}
+
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>

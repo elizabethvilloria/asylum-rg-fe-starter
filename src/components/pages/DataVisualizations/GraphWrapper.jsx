a0040ec13111d4c218fb61b51b45ef2d1370be6c
elizabethvilloria <lizvilloria@gmail.com>
2024-08-07 10:57:07 +0200

    Finished Ticket 2

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-// #1: Imported useEffect and useState
+import React from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import CitizenshipMapAll from './Graphs/CitizenshipMapAll';
@@ -11,7 +10,8 @@ import YearLimitsSelect from './YearLimitsSelect';
 import ViewSelect from './ViewSelect';
 import axios from 'axios';
 import { resetVisualizationQuery } from '../../../state/actionCreators';
-import test_data from '../../../data/test_data.json';
+// commented out
+// import test_data from '../../../data/test_data.json';
 import { colors } from '../../../styles/data_vis_colors';
 import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
 
@@ -20,16 +20,10 @@ const { background_color } = colors;
 function GraphWrapper(props) {
   const { set_view, dispatch } = props;
   let { office, view } = useParams();
-  // #2: State Variable for storing fetched data, loading state, and error state
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
   if (!view) {
     set_view('time-series');
     view = 'time-series';
   }
-  
   let map_to_render;
   if (!office) {
     switch (view) {
@@ -57,62 +51,85 @@ function GraphWrapper(props) {
         break;
     }
   }
-  function updateStateWithNewData(years, view, office, stateSettingCallback) {
-    /*
-          _                                                                             _
-        |                                                                                 |
-        |   Example request for once the `/summary` endpoint is up and running:           |
-        |                                                                                 |
-        |     `${url}/summary?to=2022&from=2015&office=ZLA`                               |
-        |                                                                                 |
-        |     so in axios we will say:                                                    |
-        |                                                                                 |     
-        |       axios.get(`${url}/summary`, {                                             |
-        |         params: {                                                               |
-        |           from: <year_start>,                                                   |
-        |           to: <year_end>,                                                       |
-        |           office: <office>,       [ <-- this one is optional! when    ]         |
-        |         },                        [ querying by `all offices` there's ]         |
-        |       })                          [ no `office` param in the query    ]         |
-        |                                                                                 |
-          _                                                                             _
-                                   -- Mack 
-    
-    */
 
+  // Function to fetch new data from the API and update the component state.
+  async function updateStateWithNewData(years, view, office, stateSettingCallback) {
+
+    const URL = "https://hrf-asylum-be-b.herokuapp.com/cases";
+
+    // Check if the office parameter is 'all' or not provided.
     if (office === 'all' || !office) {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+
+      Promise.all([
+        await axios.get(`${URL}/fiscalSummary`, {
+
           params: {
             from: years[0],
             to: years[1],
           },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    } else {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+        }),
+
+        await axios.get(`${URL}/citizenSummary`, {
+
           params: {
             from: years[0],
             to: years[1],
             office: office,
           },
         })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+      ])
+        .then(([callA, callB]) => {
+          // Getting data from the response of the first request.
+          const yearResults = callA.data.yearResults;
+
+          // Getting data from the response of the second request.
+          const citizenshipResults = callB.data;
+
+          // Combining the obtained data.
+          const combinedData = [{yearResults, citizenshipResults}];
+
+          // Update the component state with the new data.
+          stateSettingCallback(view, office, [combinedData][0]);
         })
         .catch(err => {
+          // Log any errors encountered during the data fetching process.
           console.error(err);
         });
-    }
+        }
   }
+      // axios
+      //   .get(process.env.REACT_APP_API_URI, {
+      //     // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+      //     params: {
+      //       from: years[0],
+      //       to: years[1],
+      //     },
+      //   })
+      //   .then(result => {
+      //     stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+      //   })
+      //   .catch(err => {
+      //     console.error(err);
+      //   });
+  //   } else {
+  //     axios
+  //       .get(process.env.REACT_APP_API_URI, {
+  //         // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+  //         params: {
+  //           from: years[0],
+  //           to: years[1],
+  //           office: office,
+  //         },
+  //       })
+  //       .then(result => {
+  //         stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+  //       })
+  //       .catch(err => {
+  //         console.error(err);
+  //       });
+  //   }
+  // }
+
   const clearQuery = (view, office) => {
     dispatch(resetVisualizationQuery(view, office));
   };

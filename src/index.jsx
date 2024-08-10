440060afeef9df0ac7ca029df209f271fbf16814
elizabethvilloria <lizvilloria@gmail.com>
2024-08-10 16:28:59 +0200

    Login, logout, signup button

--- src/index.jsx
+++ src/index.jsx
@@ -23,8 +23,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './state/reducers';
 import { colors } from './styles/data_vis_colors';
-import { Auth0Provider } from '@auth0/auth0-react'; // Import Auth0Provider
-import { useAuth0 } from '@auth0/auth0-react'; // Import useAuth0
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
 import Profile from './components/pages/Profile/Profile';
 
 const { primary_accent_color } = colors;
@@ -37,20 +36,12 @@ ReactDOM.render(
     {console.log('Auth0 Domain:', process.env.REACT_APP_AUTH0_DOMAIN)}
     {console.log('Auth0 Client ID:', process.env.REACT_APP_AUTH0_CLIENT_ID)}
 
-      {/* Authorization wrapper for the App */}
-      <Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-        authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}>
-        
-
       <React.StrictMode>
-        <App />
+        <Auth0ProviderWithHistory>
+          <App />
+        </Auth0ProviderWithHistory>
       </React.StrictMode>
     
-    </Auth0Provider>
     </Provider>
   </Router>,
   document.getElementById('root')
@@ -58,7 +49,7 @@ ReactDOM.render(
 
 export function App() {
   const { Footer, Header } = Layout;
-  const { isAuthenticated } = useAuth0();
+  // const { isAuthenticated } = useAuth0();
 
   return (
     <Layout>
@@ -75,9 +66,9 @@ export function App() {
       <Switch>
         <Route path="/" exact component={LandingPage} />
         <Route path="/graphs" component={GraphsContainer} />
-        {isAuthenticated && (
+        {/* {isAuthenticated && (
           <Route path="/profile" component={Profile} />
-        )}
+        )} */}
         <Route component={NotFoundPage} />
       </Switch>
       <Footer
